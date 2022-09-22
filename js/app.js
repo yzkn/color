a7394cbc7d4a4b0252c5b5efafdb065d59f334d5
YA <YA-androidapp@users.noreply.github.com>
2022-09-22 12:58:20 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,14 @@
 // Copyright (c) 2022 YA-androidapp(https://github.com/YA-androidapp) All rights reserved.
 
 
+const initListJs = () => {
+    var options = {
+        valueNames: ['number', 'name', 'code', 'complementary', 'invert']
+    };
+
+    new List('colors', options);
+}
+
 window.addEventListener('DOMContentLoaded', (event) => {
     var tableElem = document.getElementById('color-list');
 
@@ -9,21 +17,28 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
         var cellNum = trElem.insertCell(0);
         cellNum.appendChild(document.createTextNode(index));
+        cellNum.setAttribute("class", "number");
 
         var cellName = trElem.insertCell(1);
         cellName.appendChild(document.createTextNode(item.colorname));
         cellName.style.cssText = "background-color: " + item.colorname + "; color: " + item.invert + ";";
+        cellNum.setAttribute("class", "name");
 
         var cellCode = trElem.insertCell(2);
         cellCode.appendChild(document.createTextNode(item.colorcode));
         cellCode.style.cssText = "background-color: " + item.colorname + "; color: " + item.invert + ";";
+        cellNum.setAttribute("class", "code");
 
         var cellComp = trElem.insertCell(3);
         cellComp.appendChild(document.createTextNode(item.complementary));
         cellComp.style.cssText = "color: " + item.colorname + "; background-color: " + item.complementary + ";";
+        cellNum.setAttribute("class", "complementary");
 
         var cellInve = trElem.insertCell(4);
         cellInve.appendChild(document.createTextNode(item.invert));
         cellInve.style.cssText = "color: " + item.colorname + "; background-color: " + item.invert + ";";
+        cellNum.setAttribute("class", "invert");
     });
+
+    initListJs();
 });
